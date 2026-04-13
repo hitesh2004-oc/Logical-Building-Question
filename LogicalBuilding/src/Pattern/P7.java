f9f69eae40e1be81bd5825f4a0fe5994c098d455
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-04-13 11:39:26 +0530

    Pattern Practice

---
+++ LogicalBuilding/src/Pattern/P7.java
@@ -0,0 +1,16 @@
+package Pattern;
+
+public class P7 {
+	public static void main(String[] args) {
+		int n = 6;
+		for(int i = 1; i<= n; i++) {
+			for(int j = 1; j<=n;j++) {
+				if(i == 1 && j != 1 && j != n || j == 1 && i != 1 && i != n
+						|| j == n && i != 1 && i != n || i == n  && j != 1 && j != n) System.out.print("* ");
+				else System.out.print("  ");
+			}
+				System.out.println();
+		}
+			}
+
+}
